e5cfdab28b754f1ca398e97fb437330885d725ec
Xavier Viel <Xavier.s.viel@gmail.com>
2024-03-21 15:27:54 -0300

    working vigenere

--- chiffrage_dechiffrage.py
+++ chiffrage_dechiffrage.py
@@ -21,7 +21,11 @@ def cesar_encryption (message, offset = 1):
 
 		# print(encrypted_message)
 		else:
-			encrypted_message += char
+			if  ord(char) == 10:
+				pass
+			else:
+				encrypted_message += char
+			
 	return encrypted_message
 
 # print(cesar_encryption("I LOVE NATURE  yes", 1)) # J!MPWF!OBUVSF
@@ -41,20 +45,26 @@ def cesar_decryption (encrypted_message, offset = 1):
 			if newcharNumer< ord('a'):
 				newcharNumer = newcharNumer-26
 			message += chr(newcharNumer)
+
 		else:
-			message += char
+			if ord(char) == 10:
+				pass
+			else:
+				message += char
 	return message
 
-print (cesar_decryption("J MPWF OBUVSF", 1)) # J!MPWF!OBUVSF
+# print (cesar_decryption("J MPWF OBUVSF", 1)) # J!MPWF!OBUVSF
 
 #Function to encode
 def encode(key, clear):
+	clear = clear.replace("\n", "")
 	enc = []
 
 	for i in range(len(clear)):
+
 		key_c = key[i % len(key)]
 		enc_c = chr((ord(clear[i]) +
-					ord(key_c)) % 256)
+				ord(key_c)) % 256)
 
 		enc.append(enc_c)
 
@@ -68,9 +78,9 @@ def decode(key, enc):
 	for i in range(len(enc)):
 		key_c = key[i % len(key)]
 		dec_c = chr((256 + ord(enc[i]) -
-						ord(key_c)) % 256)
+					ord(key_c)) % 256)
 
 		dec.append(dec_c)
-	return "".join(dec)
-# print(encode("sdf", "Hello world")) #w5vDicOSw5_Dk8KGw6rDk8OYw5_DiA==
-# print(decode("sdf", "wrvDicOSw5_Dk8KGw6rDk8OYw5_DiA==")) #Hello world
+	return "".join(dec).replace("\n", "")
+print(encode("sdf", "Hello world")) #w5vDicOSw5_Dk8KGw6rDk8OYw5_DiA==
+print(decode("sdf", "wrvDicOSw5_Dk8KGw6rDk8OYw5_DiA==")) #Hello world
